d999df4dd7e42b1d37ab01a5d0476d066d0f4487
transcenddev <97653516+r3kashi@users.noreply.github.com>
2024-02-07 14:57:23 +0800

    Added files

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+
+onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+};
+
+function changeImage() {
+  var yesButton = document.querySelector('.button__yes');
+  yesButton.src = './assets/yes-check.png'; // Change the source of the image
+  // Change window location to the desired HTML page
+  setTimeout(function() {
+        // Change window location to the desired HTML page
+        window.location.href = 'flowers.html';
+      }, 1000); // 3000 milliseconds = 3 seconds
+}
+
+// Add event listener to the "No" button image
+document.getElementById('noButton').addEventListener('click', function() {
+  alert('Bawal Tumanggi!'); // Show an alert message
+});
+
